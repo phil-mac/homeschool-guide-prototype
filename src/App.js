af9c4c1b4213eddc57ee93b7f26f1498793c30b4
Phillip MacEachron <phillipmaceachron@Phillips-MacBook-Pro.local>
2020-01-29 13:01:19 -0800

    moved project and module selection to side drawer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Container, Paper, Grid, makeStyles} from '@material-ui/core';
+import {Container, Paper, Grid, makeStyles, Box, Drawer, CssBaseline, AppBar, Toolbar, Divider, Typography} from '@material-ui/core';
 
 import TopBar from './components/TopBar';
 import Guide from './components/Guide';
@@ -8,13 +8,33 @@ import ModuleSelect from './components/ModuleSelect';
 
 import {projects} from './exampleData';
 
+const drawerWidth = 340;
+
 const useStyles = makeStyles(theme => ({
   root: {
+    display: "flex"
+  },
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0
+  },
+  drawerPaper: {
+    width: drawerWidth,
+    background: 'lightgrey'
+  },
+  toolbar: theme.mixins.toolbar,
+  content: {
     flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing(3)
   },
   paper: {
     padding: theme.spacing(2),
-    color: theme.palette.text.secondary,
+    margin: theme.spacing(2),
   },
 }));
 
@@ -40,27 +60,69 @@ export default() => {
   }
 
   return (
-    <div style={{background: '#E2E8F0', minHeight:'100vh'}}>
-      <TopBar/>
-      <Container maxWidth='md' style={{paddingTop: '20px'}}>
-        <Grid container spacing={3}>
-          <Grid item xs={12} sm={6}>
-            <Paper className={classes.paper}>
-              <ProjectSelect projects={projects} handleChangeProject={handleChangeProject}/>
-            </Paper>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <Paper className={classes.paper}>
-              <ModuleSelect projectModules={projectModules} activeProject={activeProject} handleSetModules={handleSetModules}/>
-            </Paper>
-          </Grid>
-          <Grid item xs={12}>
-            <Paper className={classes.paper}>
-              <Guide projects={projects} activeProject={activeProject} activeModules={activeModules}/>
-            </Paper>
-          </Grid>
-        </Grid>
-      </Container>
+    <div className={classes.root}>
+      <CssBaseline />
+      {/* <TopBar className={classes.appBar}/> */}
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" noWrap>
+            Homeschool Guide Curriculum Prototype
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper
+        }}
+        anchor="left"
+      >
+        <div className={classes.toolbar} style={{background: 'lightgrey' }}>
+          <Typography variant='h5' className={classes.content} style={{background: 'lightgrey' }}>Guide Builder</Typography>
+        </div>
+        <Paper className={classes.paper}>
+          <ProjectSelect projects={projects} handleChangeProject={handleChangeProject}/>
+        </Paper>
+        <Paper className={classes.paper}>
+          <ModuleSelect projectModules={projectModules} activeProject={activeProject} handleSetModules={handleSetModules}/>
+        </Paper>
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Guide projects={projects} activeProject={activeProject} activeModules={activeModules}/>
+      </main>
     </div>
+
+    // <div style={{background: '#E2E8F0', minHeight:'100vh'}}>
+    //   <TopBar/>
+    //   <Container maxWidth='lg' style={{paddingTop: '20px'}}>
+    //     <Grid container>
+    //       <Grid container item sm={3}>
+    //         <Box position="fixed">
+    //           {/* <Grid container spacing={2}> */}
+    //             <Grid item xs={12} sm={6}>
+    //               <Paper className={classes.paper}>
+    //                 <ProjectSelect projects={projects} handleChangeProject={handleChangeProject}/>
+    //               </Paper>
+    //             </Grid>
+    //             <Grid item xs={12} sm={6}>
+    //               <Paper className={classes.paper}>
+    //                 <ModuleSelect projectModules={projectModules} activeProject={activeProject} handleSetModules={handleSetModules}/>
+    //               </Paper>
+    //             </Grid>
+    //           {/* </Grid> */}
+    //         </Box>
+    //       </Grid>
+    //       <Grid container item sm={9}>
+    //         <Grid item xs={12}>
+    //           <Paper className={classes.paper}>
+    //             <Guide projects={projects} activeProject={activeProject} activeModules={activeModules}/>
+    //           </Paper>
+    //         </Grid>
+    //       </Grid>
+    //     </Grid>
+    //   </Container>
+    // </div>
   );
 }
